30b2d2ff2d10caddef40707f08dedf784f4e9abb
boboto <radio13dev@gmail.com>
2025-07-08 18:40:56 +1000

    Colliders improved

--- Assets/root/Runtime/Projectile/ProjectileCollisionSystem.cs
+++ Assets/root/Runtime/Projectile/ProjectileCollisionSystem.cs
@@ -13,167 +13,57 @@ using UnityEngine;
 
 namespace Collisions
 {
-    /// <summary>
-    /// Projectile collision is predicted, only after all movement is done
-    /// </summary>
-    [UpdateAfter(typeof(MovementSystemGroup))]
-    [UpdateInGroup(typeof(PredictedSimulationSystemGroup))]
-    public partial class ProjectileCollisionSystemGroup : ComponentSystemGroup
-    {
-    }
-
-    [GhostComponent]
-    [BurstCompile]
-    public struct Collider : IComponentData
-    {
-        public NativeTrees.AABB2D Value;
-
-        public Collider(AABB2D value)
-        {
-            Value = value;
-        }
-
-        [BurstCompile]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        [Pure]
-        public NativeTrees.AABB2D Add(float2 offset)
-        {
-            return new NativeTrees.AABB2D(Value.min + offset, Value.max + offset);
-        }
-    }
-
-    public struct Survivor : IComponentData
-    {
-        public const int Index = 0;
-    }
-
-    public struct SurvivorProjectile : IComponentData
-    {
-        public const int Index = 1;
-    }
-
-    public struct Enemy : IComponentData
-    {
-        public const int Index = 2;
-    }
-
-    public struct EnemyProjectile : IComponentData
-    {
-        public const int Index = 3;
-    }
-
     [UpdateInGroup(typeof(ProjectileCollisionSystemGroup))]
-    public partial class ProjectileCollisionSystem : SystemBase
+    public partial struct ProjectileCollisionSystem : ISystem
     {
-        NativeArray<(NativeTrees.NativeQuadtree<Entity> Tree, EntityQuery Query)> m_trees;
+        NativeTrees.NativeQuadtree<Entity> m_projectileTree;
+        EntityQuery m_projectileQuery;
+        EntityQuery m_survivorQuery;
 
-        NativeTrees.NativeQuadtree<Entity> SurvivorTree => m_trees[0].Tree;
-        NativeTrees.NativeQuadtree<Entity> SurvivorProjectileTree => m_trees[1].Tree;
-        NativeTrees.NativeQuadtree<Entity> EnemyTree => m_trees[2].Tree;
-        NativeTrees.NativeQuadtree<Entity> EnemyProjectileTree => m_trees[3].Tree;
-
-        EntityQuery SurvivorQuery => m_trees[0].Query;
-        EntityQuery SurvivorProjectile => m_trees[1].Query;
-        EntityQuery EnemyQuery => m_trees[2].Query;
-        EntityQuery EnemyProjectileQuery => m_trees[3].Query;
-
-        protected override void OnCreate()
+        public void OnCreate(ref SystemState state)
         {
-            base.OnCreate();
+            m_projectileTree = new(
+                new(min: new float2(-1000, -1000), max: new float2(1000, 1000)),
+                Allocator.Persistent
+            );
 
-            RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
+            m_projectileQuery = SystemAPI.QueryBuilder().WithAll<LocalTransform, Collider>().WithAll<Projectile, EnemyProjectile>().Build();
+            state.RequireForUpdate(m_projectileQuery);
 
-            m_trees = new NativeArray<(NativeQuadtree<Entity> Tree, EntityQuery Query)>(4, Allocator.Persistent);
-            m_trees[0] = (
-                new(
-                    new(min: new float2(-1000, -1000), max: new float2(1000, 1000)),
-                    Allocator.Persistent
-                ),
-                SystemAPI.QueryBuilder().WithAll<LocalTransform, Collider>().WithAll<Survivor, Health>().Build()
-            );
-            m_trees[1] = (
-                new(
-                    new(min: new float2(-1000, -1000), max: new float2(1000, 1000)),
-                    Allocator.Persistent
-                ),
-                SystemAPI.QueryBuilder().WithAll<LocalTransform, Collider>().WithAll<SurvivorProjectile>().Build()
-            );
-            m_trees[2] = (
-                new(
-                    new(min: new float2(-1000, -1000), max: new float2(1000, 1000)),
-                    Allocator.Persistent
-                ),
-                SystemAPI.QueryBuilder().WithAll<LocalTransform, Collider>().WithAll<Enemy, Health>().Build()
-            );
-            m_trees[3] = (
-                new(
-                    new(min: new float2(-1000, -1000), max: new float2(1000, 1000)),
-                    Allocator.Persistent
-                ),
-                SystemAPI.QueryBuilder().WithAll<LocalTransform, Collider>().WithAll<EnemyProjectile>().Build()
-            );
-            
-            for (int i = 0; i < m_trees.Length; i++)
-                RequireAnyForUpdate(m_trees[i].Query);
+            m_survivorQuery = SystemAPI.QueryBuilder().WithAll<LocalTransform, Collider>().WithAll<Survivor, Health>().Build();
+            state.RequireForUpdate(m_survivorQuery);
         }
 
-        protected override void OnDestroy()
-        {
-            base.OnDestroy();
-
-            for (int i = 0; i < m_trees.Length; i++)
-            {
-                m_trees[i].Tree.Dispose();
-            }
-
-            m_trees.Dispose();
-        }
-
-        protected override void OnUpdate()
+        public void OnUpdate(ref SystemState state)
         {
             // Update trees
-            RegenerateJob[] jobs = new RegenerateJob[m_trees.Length];
-            for (int i = 0; i < m_trees.Length; i++)
+            var projectile_entities = m_projectileQuery.ToEntityArray(allocator: Allocator.TempJob);
+            var projectile_colliders = m_projectileQuery.ToComponentDataArray<Collider>(allocator: Allocator.TempJob);
+            var projectile_transforms = m_projectileQuery.ToComponentDataArray<LocalTransform>(allocator: Allocator.TempJob);
+            state.Dependency = new RegenerateJob()
             {
-                jobs[i] = new RegenerateJob()
-                {
-                    tree = m_trees[i].Tree,
-                    entities = m_trees[i].Query.ToEntityArray(allocator: Allocator.TempJob),
-                    colliders = m_trees[i].Query.ToComponentDataArray<Collider>(allocator: Allocator.TempJob),
-                    transforms = m_trees[i].Query.ToComponentDataArray<LocalTransform>(allocator: Allocator.TempJob),
-                };
-                this.Dependency = jobs[i].Schedule(this.Dependency);
-            }
+                tree = m_projectileTree,
+                entities = projectile_entities,
+                colliders = projectile_colliders,
+                transforms = projectile_transforms
+            }.Schedule(state.Dependency);
 
             // Wait for that
-            this.CompleteDependency();
-
-            for (int i = 0; i < jobs.Length; i++)
-            {
-                jobs[i].entities.Dispose();
-                jobs[i].colliders.Dispose();
-                jobs[i].transforms.Dispose();
-            }
+            state.CompleteDependency();
 
             // Perform collisions
-            var delayedEcb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(World.Unmanaged);
+            var delayedEcb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
             var parallel = delayedEcb.AsParallelWriter();
-
-            // ~~ Survivor Hit Checks ~~
-            this.Dependency = new CollisionJob()
+            new CollisionJob()
             {
                 ecb = parallel,
-                tree = EnemyProjectileTree
-            }.Schedule(SurvivorQuery, this.Dependency);
+                tree = m_projectileTree,
+                projectileLookup = SystemAPI.GetComponentLookup<Projectile>(false)
+            }.Schedule(m_survivorQuery);
 
-            // ~~ Enemy Hit Checks ~~
-            this.Dependency = new CollisionJob()
-            {
-                ecb = parallel,
-                tree = SurvivorProjectileTree
-            }.Schedule(EnemyQuery, this.Dependency);
-
-            this.CompleteDependency();
+            projectile_entities.Dispose();
+            projectile_colliders.Dispose();
+            projectile_transforms.Dispose();
         }
 
         partial struct RegenerateJob : IJob
@@ -198,13 +88,15 @@ namespace Collisions
         {
             public EntityCommandBuffer.ParallelWriter ecb;
             [ReadOnly] public NativeTrees.NativeQuadtree<Entity> tree;
+            public ComponentLookup<Projectile> projectileLookup;
 
-            unsafe public void Execute([ChunkIndexInQuery] int Key, Entity entity, in LocalTransform transform, in Collider collider, ref Health health)
+            unsafe public void Execute([ChunkIndexInQuery] int Key, Entity entity, in LocalTransform transform, in Collider collider, ref Health survivorHealth)
             {
                 var adjustedAABB2D = collider.Add(transform.Position.xy);
-                fixed (Health* ptr = &health)
+                fixed (Health* survivorHealth_ptr = &survivorHealth)
+                fixed (ComponentLookup<Projectile>* projectileLookup_ptr = &projectileLookup)
                 {
-                    var visitor = new CollisionVisitor(Key, ref ecb, ptr);
+                    var visitor = new CollisionVisitor(Key, ref ecb, survivorHealth_ptr, projectileLookup_ptr);
                     tree.Range(adjustedAABB2D, ref visitor);
                     visitor.Dispose();
                 }
@@ -216,12 +108,14 @@ namespace Collisions
                 EntityCommandBuffer.ParallelWriter _ecb;
                 Health* _health;
                 NativeParallelHashSet<Entity> _ignoredCollisions;
+                ComponentLookup<Projectile>* _projectileLookup;
 
-                public CollisionVisitor(int key, ref EntityCommandBuffer.ParallelWriter ecb, Health* health)
+                public CollisionVisitor(int key, ref EntityCommandBuffer.ParallelWriter ecb, Health* health, ComponentLookup<Projectile>* projectileLookup)
                 {
                     _key = key;
                     _ecb = ecb;
                     _health = health;
+                    _projectileLookup = projectileLookup;
                     _ignoredCollisions = new NativeParallelHashSet<Entity>(4, Allocator.TempJob);
                 }
 
@@ -232,7 +126,7 @@ namespace Collisions
                     if (_ignoredCollisions.Add(projectile))
                     {
                         // Destroy projectiles when they collide (by setting their life to 0)
-                        _ecb.SetComponent(_key, projectile, new Projectile());
+                        _projectileLookup->GetRefRW(projectile).ValueRW.DestroyTime = 0;
                         _health->Value -= 1;
                     }
 
