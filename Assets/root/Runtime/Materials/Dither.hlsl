2dc6af1f39fc225c1844952d7bd8e2a509212ad3
boboto <radio13dev@gmail.com>
2025-07-29 19:15:34 +1000

    Artstyle!

---
+++ Assets/root/Runtime/Materials/Dither.hlsl
@@ -0,0 +1,9 @@
+#ifndef DITHER_INCLUDED
+#define DITHER_INCLUDED
+
+void Dither_float(float2 screenPosition, float3 objectWorldPosition, float3 cameraPosition, out float dither){
+	float2 floord = abs(floor(screenPosition - objectWorldPosition.xy));
+	float ditherValue = (floord.x + floord.y)%2;
+	dither = ditherValue;
+}
+#endif // SPRITE_SHEET_INCLUDED
