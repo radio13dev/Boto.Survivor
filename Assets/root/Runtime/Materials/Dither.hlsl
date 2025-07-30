a4b646d53be4913482d543f80a2ca4ed5c3c9e5c
boboto <radio13dev@gmail.com>
2025-07-30 19:37:14 +1000

    Rim light thing for enemies

--- Assets/root/Runtime/Materials/Dither.hlsl
+++ Assets/root/Runtime/Materials/Dither.hlsl
@@ -2,7 +2,7 @@
 #define DITHER_INCLUDED
 
 void Dither_float(float2 screenPosition, float3 objectWorldPosition, float3 cameraPosition, out float dither){
-	float2 floord = abs(floor(screenPosition - objectWorldPosition.xy));
+	float2 floord = abs(floor(screenPosition));
 	float ditherValue = (floord.x + floord.y)%2;
 	dither = ditherValue;
 }
