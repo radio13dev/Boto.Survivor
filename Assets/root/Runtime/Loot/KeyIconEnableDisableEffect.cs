0a0e159f9bc42d5f2192488f9e1d4e290b2e7c75
Ben Wheatley <radio13dev@gmail.com>
2025-10-07 09:27:54 +1000

    More UI work

---
+++ Assets/root/Runtime/Loot/KeyIconEnableDisableEffect.cs
@@ -0,0 +1,46 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class KeyIconEnableDisableEffect : MonoBehaviour
+{
+    private static readonly int OutlineColor = Shader.PropertyToID("_Outline_Color");
+    
+    public MeshRenderer Background;
+    public MeshRenderer Icon;
+    public TMP_Text Text;
+    public Color DisabledColorMul = new Color(0.25f, 0.25f, 0.25f, 1f);
+    
+    Color[] Colors {get => m_Colors ??= new Color[] { Background.material.color, Background.material.GetColor(OutlineColor), Icon.material.color, Text.color }; }
+    Color[] m_Colors;
+    bool m_IsDisabled;
+
+    private void Awake()
+    {
+        m_Colors = null;
+    }
+
+    [EditorButton]
+    public void SetEnabled() => Set(true);
+    
+    [EditorButton]
+    public void SetDisabled() => Set(false);
+    
+    [EditorButton]
+    public void Toggle() => Set(!m_IsDisabled);
+    
+    [EditorButton]
+    public void Set(bool v)
+    {
+        m_IsDisabled = v;
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        Background.material.color = m_IsDisabled ? DisabledColorMul*Colors[0] : Colors[0];
+        Background.material.SetColor(OutlineColor, m_IsDisabled ? DisabledColorMul*Colors[1] : Colors[1]);
+        Icon.material.color = m_IsDisabled ? DisabledColorMul*Colors[2] : Colors[2];
+        Text.color = m_IsDisabled ? DisabledColorMul*Colors[3] : Colors[3];
+    }
+}
