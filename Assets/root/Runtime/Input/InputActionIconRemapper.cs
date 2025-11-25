60bdcd38b67e8adb56078aa91edd8dd273c1d3e6
Dakota E <62161190+Kaizenoku@users.noreply.github.com>
2025-11-25 16:04:04 +1000

    Update input mode based on last used device

--- Assets/root/Runtime/Input/InputActionIconRemapper.cs
+++ Assets/root/Runtime/Input/InputActionIconRemapper.cs
@@ -18,14 +18,57 @@ public class InputActionIconRemapper : MonoBehaviour
     private static readonly int XboxProperty = Shader.PropertyToID("_Xbox");
     private static readonly int XboxOutlineProperty = Shader.PropertyToID("_XboxOutline");
     private InputSystem_Actions _inputActions;
+    private InputDevice _lastDevice;
 
     private IEnumerator Start()
     {
         yield return new WaitUntil(() => GameInput.Inputs != null);
         _inputActions = GameInput.Inputs;
         UpdateInputKeyMaterials();
+        
+        _inputActions.UI.Get().actionTriggered += OnInputActionTriggered;
+        _inputActions.Player.Get().actionTriggered += OnInputActionTriggered;
+    }
+
+    private void OnDestroy()
+    {
+        if (_inputActions == null) { return; }
+        _inputActions.UI.Get().actionTriggered -= OnInputActionTriggered;
+        _inputActions.Player.Get().actionTriggered -= OnInputActionTriggered;
     }
     
+    private void OnInputActionTriggered(InputAction.CallbackContext context)
+    {
+        if (context.canceled) { return; }
+        var currentDevice = context.control.device;
+        if (currentDevice == null || currentDevice == _lastDevice) { return; }
+        UpdateInputDevice(currentDevice);
+    }
+    
+    private void UpdateInputDevice(InputDevice newDevice)
+    {
+        _lastDevice = newDevice;
+
+        switch (newDevice)
+        {
+            // Keyboard or mouse
+            case UnityEngine.InputSystem.Keyboard or Mouse
+                when GameInput.CurrentMode != GameInput.Mode.Keyboard:
+                GameInput.SetInputMode(GameInput.Mode.Keyboard);
+                break;
+            // Any gamepad
+            case Gamepad
+                when GameInput.CurrentMode != GameInput.Mode.Xbox:
+                GameInput.SetInputMode(GameInput.Mode.Xbox);
+                break;
+            // Touch controls
+            case Touchscreen
+                when GameInput.CurrentMode != GameInput.Mode.Touch:
+                GameInput.SetInputMode(GameInput.Mode.Touch);
+                break;
+        }
+    }
+
     [EditorButton]
     private void UpdateInputKeyMaterials()
     {
